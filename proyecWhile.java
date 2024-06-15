34850ce37055ffd93060df2fd9deaa71f3608603
ayarin7 <antonybrucce04@gmail.com>
2024-06-15 14:30:22 -0500

    Update proyecWhile.java

--- proyecWhile.java
+++ proyecWhile.java
@@ -5,6 +5,7 @@ public static void main(String[] args){
     System.out.println("loca");
     System.out.println("otra");
     System.out.println("perro");
+    System.out.println("perra");
 
     }
 }
